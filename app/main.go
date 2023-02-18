0981d3f5dac0809c67fc8cfbf838b7e4da8c6386
Takuma Tomori <tmrtkm1227tksx@gmail.com>
2023-02-18 15:30:47 +0900

    ✨ Stdout, Stderrの出力を適切に分ける

--- app/main.go
+++ app/main.go
@@ -1,32 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	// Uncomment this block to pass the first stage!
 	"os"
 	"os/exec"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage!
-
-    if len(os.Args) < 4 {
-        fmt.Println("Usage: run <image> <command> <arg1> <arg2> ...")
-        os.Exit(1)
-    }
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: run <image> <command> <arg1> <arg2> ...")
+		os.Exit(1)
+	}
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(string(output))
+	if 0 < stdout.Len() {
+		fmt.Fprint(os.Stdout, string(stdout.Bytes()))
+	}
+	if 0 < stderr.Len() {
+		fmt.Fprint(os.Stderr, string(stderr.Bytes()))
+	}
 }
